78702dd8b8c8960c075fa2b515351a3c46e44948
soobinua <dev7er@gmail.com>
2023-07-13 10:01:52 +0900

    ch01 자바 시작

---
+++ src/thisisjava/ch01/sec11/Hello.java
@@ -0,0 +1,21 @@
+package thisisjava.ch01.sec11;
+
+/**
+ * 도큐먼트 주석
+ * @author soo
+ */
+/*
+ * 범위 주석
+ * 장제목 : 1장 자바 시작하기 
+ * 작성일 : 2023.07.13
+ */
+public class Hello {
+
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+		
+	}
+
+}
